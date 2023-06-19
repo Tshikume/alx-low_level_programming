dab8313c31bae2da3f0ea0b88898e2bd1049a01f
Tshikume <tshikumemoseboa@gmail.com>
2023-06-18 18:47:33 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 /**
 * main - Entry point of the program
-*
-* Return: 0 (success)
+* Return: always 0 (success)
 */
 int main(void)
 {
-printf("Size of char: %zu byte(s)\n", sizeof(char));
-printf("Size of int: %zu byte(s)\n", sizeof(int));
-printf("Size of float: %zu byte(s)\n", sizeof(float));
-printf("Size of double: %zu byte(s)\n", sizeof(double));
-printf("Size of long long: %zu byte(s)\n", sizeof(long long));
-printf("Size of unsigned int: %zu byte(s)\n", sizeof(unsigned int));
-return (0);
+printf("Size of a char : %d byte(s)\n", sizeof(char));
+printf("Size of an int : %d byte(s)\n", sizeof(int));
+printf("Size of long int : %d byte(s)\n", sizeof(long int));
+printf("Size of long long : %d byte(s)\n", sizeof(long long));
+printf("Size of float : %d byte(s)\n", sizeof(float));
 }
