c631e25ca9d68a9632c15efb7582fdfdfa4860fd
Tshikume <tshikumemoseboa@gmail.com>
2023-06-18 18:50:07 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,4 +10,5 @@ printf("Size of an int : %d byte(s)\n", sizeof(int));
 printf("Size of long int : %d byte(s)\n", sizeof(long int));
 printf("Size of long long : %d byte(s)\n", sizeof(long long));
 printf("Size of float : %d byte(s)\n", sizeof(float));
+return (0);
 }
