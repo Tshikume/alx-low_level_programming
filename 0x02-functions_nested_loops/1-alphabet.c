025fcfa3e35640ce1bae6762b52846a4aafbd126
Tshikume <tshikumemoseboa@gmail.com>
2023-06-20 15:40:27 -0700

    .

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,11 @@
 #include <unistd.h>
 /**
-* print_alphabet - a function that prints the alphabet in lowercase. 
-* return;
+* print_alphabet - a function that prints the alphabet in lowercase.
 */
-int _putchar(char c);
-
 void print_alphabet(void)
 {
 char letter;
-for (letter = 'a'; letter <= 'z'; letter++) {
+for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
-}
+_putchar('\n');
 }
