08d8e228fd2f5a64b6772ac97838fb025a98bc2c
Tshikume <tshikumemoseboa@gmail.com>
2023-06-20 13:29:04 -0700

    .

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,14 @@
 #include <unistd.h>
-
-int main() {
-    char message[] = "_putchar\n";
-    int length = sizeof(message) - 1;
-
-    write(1, message, length);
-    return 0;
+/**
+* main - a program that prints _putchar
+*
+* Return: 0 (success)
+*/
+int main(void)
+{
+char message[] = "_putchar\n";
+int length = sizeof(message) - 1;
+write(1, message, length);
+return (0);
 }
 
