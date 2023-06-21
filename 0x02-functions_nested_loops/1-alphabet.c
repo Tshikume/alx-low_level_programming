57ae2200419a75297e72617fdd7943787d1305ed
Tshikume <tshikumemoseboa@gmail.com>
2023-06-20 17:27:07 -0700

    .

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,6 @@
 
 /**
 * print_alphabet - a function that prints the alphabet in lowercase.
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
 */
 void print_alphabet(void)
 {
