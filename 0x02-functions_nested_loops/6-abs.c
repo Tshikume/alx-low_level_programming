3eae5b0a4a42fa5ef85ca8c321be64805aece511
Tshikume <tshikumemoseboa@gmail.com>
2023-06-21 15:04:00 -0700

    absolute integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _abs - a function that computes the absolute value of an integer.
+ * @c: The character is an absolute value
+ *
+ * Return: absolute value of an integer.
+*/
+int _abs(int c)
+{
+if (c >= 0)
+return (c);
+else
+return (-c);
+}
