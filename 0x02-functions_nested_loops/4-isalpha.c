8a388b5bcd5de2eee1220133b11700a0bf30b4fb
Tshikume <tshikumemoseboa@gmail.com>
2023-06-21 13:00:27 -0700

    .

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,11 +4,11 @@
 * _isalpha -  a function that checks for alphabetic characters.
  * @c: The character is alphabetic
  *
- * Return: 1 when c is a letter  and 0 on otherwise
+ * Return: 1 when c is lowercase or uppercase, 0 otherwise.
 */
 int _isalpha(int c)
 {
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+if ((c >= 'a' && c <= 'z') ||  (c >= 'A' && c <= 'Z'))
 return (1);
 return (0);
 }
