fefd15dc56e545095ef1f06d8ebb17f1e1519d43
Tshikume <tshikumemoseboa@gmail.com>
2023-06-27 09:02:12 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *int _strlen - returns the length of a string
+ *@s - string to return lenghth of
+ *Return: length
+ */
+int _strlen(char *s)
+{
+int length = 0;
+while (str[length] != '\0') 
+{
+length++;
+}
+return (length);
+}
