e42ab948f1948380141781c7fe7bf3783d8bda15
Tshikume <tshikumemoseboa@gmail.com>
2023-06-29 17:55:51 -0700

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,24 +4,26 @@
  * _strcat - a function that concatenates two strings
  * @dest: string 1
  * @src: string 2
- *Return: dest
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
-int i = 0;
-int j = 0;
-
+int i;
+int j;
+ 
+i = 0;
 while (dest[i] != '\0')
 {
 i++;
 }
-
+j = 0;
 while (src[j] != '\0')
 {
 dest[i] = src[j];
 i++;
 j++;
 }
+ 
 dest[i] = '\0';
 return (dest);
 }
