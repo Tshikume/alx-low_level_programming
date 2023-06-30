4c3303cadab5a4bb623d2e41cbf8479ab13ea117
Tshikume <tshikumemoseboa@gmail.com>
2023-06-30 15:48:04 -0700

    encoding

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,50 +1,24 @@
 #include "main.h"
-
 /**
- * leet - encode a string into 1337
- * @s: string
- * Return: encoded string `s`
- */
-
-char *leet(char *str){
-    char *encoded = malloc(strlen(str) + 1);
-    if (encoded == NULL) {
-        // Memory allocation failed
-        return NULL;
-    }
-
-    const char *letters = "aeotl";
-    const char *replacements = "43071";
-
-    int i, j;
-    for (i = 0; str[i] != '\0'; i++) {
-        encoded[i] = str[i];
-        for (j = 0; letters[j] != '\0'; j++) {
-            if (str[i] == letters[j] || str[i] == letters[j] - 32) {
-                encoded[i] = replacements[j];
-                break;
-            }
-        }
-    }
-    encoded[i] = '\0';
-
-    return encoded;
+* leet - encode into 1337speak
+* @n: input value
+* Return: n value
+*/
+char *leet(char *n)
+{
+int i, j;
+char s1[] = "aAeEoOtTlL";
+char s2[] = "4433007711";
+ 
+for (i = 0; n[i] != '\0'; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (n[i] == s1[j])
+{
+n[i] = s2[j];
 }
-
-int main() {
-    char *str = "Hello, World!";
-    char *encoded = leet(str);
-
-    // Output the encoded string to the console
-    int i = 0;
-    while (encoded[i] != '\0') {
-        // Output the encoded character to the console
-        putchar(encoded[i]);
-        i++;
-    }
-
-    // Free the allocated memory
-    free(encoded);
-
-    return 0;
+}
+}
+return (n);
 }
