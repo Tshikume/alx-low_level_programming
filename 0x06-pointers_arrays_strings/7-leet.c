421f74f9bbf1f88cde4c930c7dc00677207f1848
Tshikume <tshikumemoseboa@gmail.com>
2023-06-30 08:57:35 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,23 +6,46 @@
  * Return: encoded string `s`
  */
 
-char *leet(char *s)
+char *leet(char *str)
 {
-	int i;
+    char *encoded = malloc(strlen(str) + 1);
+    if (encoded == NULL) {
+        // Memory allocation failed
+        return NULL;
+    }
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
-	}
+    const char *letters = "aeotl";
+    const char *replacements = "43071";
 
-	return (s);
+    int i, j;
+    for (i = 0; str[i] != '\0'; i++) {
+        encoded[i] = str[i];
+        for (j = 0; letters[j] != '\0'; j++) {
+            if (str[i] == letters[j] || str[i] == letters[j] - 32) {
+                encoded[i] = replacements[j];
+                break;
+            }
+        }
+    }
+    encoded[i] = '\0';
+
+    return encoded;
+}
+
+int main() {
+    char *str = "Hello, World!";
+    char *encoded = leet(str);
+
+    // Output the encoded string to the console
+    int i = 0;
+    while (encoded[i] != '\0') {
+        // Output the encoded character to the console
+        putchar(encoded[i]);
+        i++;
+    }
+
+    // Free the allocated memory
+    free(encoded);
+
+    return 0;
 }
