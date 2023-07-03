7c8fc1ef61b2d4d78bdf76387dbbac30d83a0634
Tshikume <tshikumemoseboa@gmail.com>
2023-07-03 15:58:17 -0700

    bytes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,9 +6,9 @@
 *
 * Return: changed array with new value for n bytes
 */
-char*_ memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n);
 {
-int i= 0;
+int i = 0;
 for (; n > 0; i++)
 {
 s[i] = b;
