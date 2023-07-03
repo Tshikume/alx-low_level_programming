02ed70662d3f14ee77ff6f889ad2c7299e9d91f5
Tshikume <tshikumemoseboa@gmail.com>
2023-07-03 16:24:41 -0700

    string character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates the first occurrence of a character in a string
+ * @s: Input string
+ * @c: Character to locate
+ *
+ * Return: Pointer to the first occurrence of the character,
+ *         or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 0-memset.c is a function that fills memory with a constant byte
 1-memcpy.c is a function that locates a character in a string
+2-strchr.c is a function that locates a character in a string.
