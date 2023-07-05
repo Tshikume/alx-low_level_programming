570fb9abd2529f8af6967a0ce6a588f80512b750
Tshikume <tshikumemoseboa@gmail.com>
2023-07-05 16:37:38 -0700

    recursive string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,20 @@
 #include "main.h"
 
 /**
- *_puts_recursion - Prints a string followed by a new line.
- *@st: The string to be printed.
+ * _putchar - Writes a character to the standard output
+ * @c: The character to be written
+ *
+ * Return: On success, returns the character written.
+ * On error, returns -1 and sets errno appropriately.
+ */
+int _putchar(char c)
+{
+return (putchar(c));
+}
+
+/**
+ * _puts_recursion - Prints a string followed by a new line recursively.
+ * @st: The string to be printed.
  */
 void _puts_recursion(char *st)
 {
