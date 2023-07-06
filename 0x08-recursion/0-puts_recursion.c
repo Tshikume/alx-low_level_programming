97930c5512d3961e2ee49d42b9a30bcaff639833
Tshikume <tshikumemoseboa@gmail.com>
2023-07-05 18:35:44 -0700

    recursive string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,13 +3,13 @@
 /**
  * _puts_recursion - Prints a string followed by a new line
  * @s: input
- * return: always 0 (Success)
+ *
  */
 void _puts_recursion(char *s)
 {
-if (*s)
+if (s[0] != '\0')
 {
-_putchar(*s);
+_putchar(s[0]);
 _puts_recursion(s + 1);
 }
 else
